78fadda2d1e35d89690edfa9d9f925271e10872a
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2023-11-01 21:09:16 +0700

    module version

--- app.py
+++ app.py
@@ -2,134 +2,115 @@
 Create a Flask app for web application chatbot using engine openai
 - Framework web application: Flask
 - Framework frontend web application: Vue.js
+- Framework chatbot: OpenAI
 - Storage data local a simple
 
 Detail:
 - Flask app: app.py
 - Vue.js app: index.html using CDN
-- Storage data: data.json
-- Fetch to this api https://api.openai.com/v1/chat/completions condition is Qestion and Answer for chatbot
+- Create a function for generate response from OpenAI model: generate_response(prompt, model_engine)
+- Create a function for API routes: chatbot(), get_data()
+- Save data "question" and "answer": history.json 
+- Save "message" from user and chatbot: data.json
 
-Example:
-curl https://api.openai.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-    "model": "gpt-3.5-turbo",
-    "messages": [
-      {
-        "role": "system",
-        "content": "You are a helpful assistant." # Answer
-      },
-      {
-        "role": "user",
-        "content": "Hello!" # Question
-      }
-    ]
-  }'
+history.json (example):
+[{'question': 'Hello', 'answer': 'Hi'}]
 
-*make api for chatbot from example above
+data.json:
+[{'role': 'user', 'content': 'Hello!'}] 
 
 APIs:
-- GET / return index.html
-- GET /get-data return data.json
-- POST /chatbot payload: {message: 'Hello!'} return {message: 'Answer', role: 'system'}
+- GET "/" return index.html
+- GET "/get-data" return data.json
+- POST "/chatbot payload" {message: 'Hello!'} return {answer: 'Answer from model'}
+
+Example for call model "gpt-3.5-turbo":
+    >>> openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages # import from data.json
+        )
 
 Author: Watcharapon Weeraborirak
 """
 
-# Import library
-import os
-import json
+from flask import Flask, request, jsonify, render_template
 import openai
-import requests
-from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
+import json
+import os
 
-# Create Flask app
 app = Flask(__name__)
 
-# Set key openai
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.organization = os.getenv("OPENAI_ORGANIZATION_ID")
+# Set environment variable OPENAI_API_KEY
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# Set environment
-load_dotenv()
+import os
 
+# Create history.json and data.json if they don't exist
+if not os.path.exists('history.json'):
+    with open('history.json', 'w') as f:
+        f.write('[]')
 
-# Message is undefined it error frontend
-if not os.path.exists("data.json"):
-    with open("data.json", "w") as f:
-        json.dump([], f)
+if not os.path.exists('data.json'):
+    with open('data.json', 'w') as f:
+        f.write('[]')
 
-
-# Route index
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-# Route to get data from json
-@app.route("/get-data")
-def get_data():
-    with open("data.json", "r") as f:
-        data = json.load(f)
-    return jsonify(data)
+# Load history and data
+with open('history.json', 'r') as f:
+    history = json.load(f)
+with open('data.json', 'r') as f:
+    data = json.load(f)
 
 
-# Route chatbot
-@app.route("/chatbot", methods=["POST"])
-def chatbot():
-
-    # Get data from frontend
-    data = request.get_json()
-    question = data["message"]
-
-    # Set data for openai api
-    data_openai = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {
-                "role": "system",
-                "content": "You are a helpful assistant.",
-            },
-            {
-                "role": "user",
-                "content": question,
-            },
-        ],
-    }
-
-    # Set header for openai api
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + os.getenv("OPENAI_API_KEY"),
-    }
-
-    # Send request to openai api
-    response = requests.post(
-        "https://api.openai.com/v1/chat/completions",
-        headers=headers,
-        data=json.dumps(data_openai),
+def generate_response(prompt, model_engine="gpt-3.5-turbo"):
+    response = openai.ChatCompletion.create(
+        model=model_engine,
+        messages=prompt
     )
+    return response
 
-    # Get data from openai api
-    data_response = response.json()
-    answer = data_response['choices'][-1]['message']['content']
-    
-    # Get data from data.json
-    with open("data.json", "r") as f:
-        data = json.load(f)
 
-    data.append({"question": question, "answer": answer})
+@app.route('/')
+def home():
+    """
+    Renders the index.html template for the home page.
 
-    # Save data to data.json
-    with(open("data.json", "w")) as f:
+    Returns:
+        The rendered index.html template.
+    """
+    return render_template('index.html')
+
+
+@app.route('/get-data', methods=['GET'])
+def get_data():
+    """
+    Returns the history data in JSON format.
+    """
+    return jsonify(history)
+
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    message = request.json['message']
+
+    # Save data
+    data.append({'role': 'user', 'content': message})
+
+    response = generate_response(data)
+    answer = response['choices'][-1]['message']['content']
+
+    # Save history
+    history.append({'question': message, 'answer': answer})
+
+    # Save to file data.json
+    with open('data.json', 'w') as f:
         json.dump(data, f)
+
+    # Save to file history.json
+    with open('history.json', 'w') as f:
+        json.dump(history, f)
 
     return jsonify({'answer': answer})
 
 
-
-# Run app
 if __name__ == "__main__":
     app.run(debug=True)
